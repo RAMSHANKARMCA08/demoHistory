ba7e221569232e98d65a05ca5e4ef8c1a18813c4
RamShankar For Class <ramshankar.mca08@gmail.com>
2020-04-19 13:02:09 +0530

    First

---
+++ auto.bat
@@ -0,0 +1,34 @@
+git init
+touch file1.txt
+git add .
+git commit -m "First"
+touch file2.txt
+git add .
+git commit -m "Second"
+touch file3.txt
+git add .
+git commit -m "Third"
+touch file4.txt
+git add .
+git commit -m "Fourth"
+touch file5.txt
+git add .
+git commit -m "Fifth"
+touch file6.txt
+git add .
+git commit -m "Sixth"
+touch file7.txt
+git add .
+git commit -m "Seventh"
+touch file8.txt
+git add .
+git commit -m "Eighth"
+touch file9.txt
+git add .
+git commit -m "Nineth"
+touch file10.txt
+git add .
+git commit -m "Tenth"
+git remote add origin https://github.com/RAMSHANKARMCA08/demoHistory.git
+git push --set-upstream origin master
+git push
